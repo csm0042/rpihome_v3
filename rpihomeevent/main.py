7e3ca99d9b0a9fda417187ff0c974bf37ab5314a
Maue <chris.maue@intelligrated.com>
2017-02-14 16:28:26 -0600

    Initial commit

--- rpihomeevent/main.py
+++ rpihomeevent/main.py
@@ -1,30 +1,35 @@
 import asyncio
 import time
+import datetime
+
 
 
 def hello_world(loop):
-    print('Hello World')
+    print('Hello World ' + str(datetime.datetime.now().time()))
     loop.call_later(1, hello_world, loop)
 
 def good_evening(loop):
-    print('Good Evening')
+    print('Good Evening ' + str(datetime.datetime.now().time()))
     loop.call_later(1, good_evening, loop)
 
 @asyncio.coroutine
 def hello_world_coroutine():
     while True:
-        yield from asyncio.sleep(1)
-        print('Hello World Coroutine')
+        yield from asyncio.sleep(0.5)
+        print('Hello World Coroutine ' + str(datetime.datetime.now().time()))
 
 @asyncio.coroutine
 def good_evening_coroutine():
     while True:
-        yield from asyncio.sleep(1)
-        print('Good Evening Coroutine')
+        yield from asyncio.sleep(0.5)
+        print('Good Evening Coroutine ' + str(datetime.datetime.now().time()))
 
+
+# Get main event loop
 print('step: asyncio.get_event_loop()')
 loop = asyncio.get_event_loop()
 
+# Call functions from main loop
 print('step: loop.call_soon(hello_world, loop)')
 loop.call_soon(hello_world, loop)
 print('step: loop.call_soon(good_evening, loop)')
