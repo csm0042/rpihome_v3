e1af6aa527830a4cc6e170f23047f5fa8eb0ad55
Maue <chris.maue@intelligrated.com>
2017-03-15 09:43:27 -0500

    Event loop created in main

---
+++ run.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" main.py:
+    Main entry-point into the RPiHome application.
+"""
+
+# Import Required Libraries (Standard, Third Party, Local) ********************
+import asyncio
+import datetime
+import logging
+import typing
+
+import rpihome_v3
+
+
+# Authorship Info *************************************************************
+__author__ = "Christopher Maue"
+__copyright__ = "Copyright 2017, The RPi-Home Project"
+__credits__ = ["Christopher Maue"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Christopher Maue"
+__email__ = "csmaue@gmail.com"
+__status__ = "Development"
+
+
+# Event Loop Scheduler ********************************************************
+def event_scheduler(loop, refresh, logger, p_devices, a_devices):
+    # do some stuff
+    logger.debug('Running scheduler')
+    loop.call_later(
+        refresh, event_scheduler,
+        loop, refresh, logger, p_devices, a_devices)
+
+
+# Main event loop function ****************************************************
+def main():
+    """ main function for the rpihome application """
+
+    # Get configuration from INI file for this run
+    logger, p_devices, a_devices = rpihome_v3.configure_all(
+        'rpihome_v3/config.ini')
+    logger.info(
+        'RpiHome-v3 Application Started @ [%s] ******************************',
+        str(datetime.datetime.now()))
+    logger.info('Configuraiton imported from INI file')
+
+    # Get main event loop *****************************************************
+    logger.info('Getting main event loop')
+    event_loop = asyncio.get_event_loop()
+
+    # Schedule call to main scheduler function ********************************
+    logger.info('Call soon event_scheduler')
+    event_loop.call_soon(
+        event_scheduler,
+        event_loop, 1.0, logger, p_devices, a_devices)
+
+    # Run event loop until keyboard interrupt received ************************
+    try:
+        logger.info('Call run_forever on main event loop')
+        event_loop.run_forever()
+        logger.info('Main event loop is started')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        logger.info(
+            'Main event loop terminated @ [%s] ******************************',
+            str(datetime.datetime.now()))
+        event_loop.close()
+
+
+# Call as script if run as __main__ *******************************************
+if __name__ == '__main__':
+    main()
