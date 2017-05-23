d787c22fd88550a8d4d470e569be4a01ae5837b2
Maue <chris.maue@intelligrated.com>
2017-05-23 14:53:40 -0500

    Converted wemo API to a class

--- rpihome_v3/wemo.py
+++ rpihome_v3/wemo.py
@@ -22,188 +22,192 @@ __email__ = "csmaue@gmail.com"
 __status__ = "Development"
 
 
-# Wemo discover / connect function ********************************************
-async def wemo_discover(device, logger):
-    """ discovers wemo device on network based upon known IP address """
-    # Regular expression for a valid IPv4 address
+# IPv4 Format helper function *************************************************
+def check_ipv4(address):
     ipv4_regex = r'\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
                  r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
                  r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
                  r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
-
-    # Check if valid IP address was provided in device attributes
-    if re.fullmatch(ipv4_regex, device.address) is not None:
-        logger.debug('Valid IP address provided')
-        # Attempt to discover wemo device
-        try:
-            wemo_device = None
-            wemo_port = pywemo.ouimeaux_device.probe_wemo(device.address)
-            logger.debug('Device discovered at port %s', wemo_port)
-        except:
-            wemo_port = None
-            logger.debug('Failed to discover port for [%s]', device.name)
+    if re.fullmatch(ipv4_regex, address) is not None:
+        return True
     else:
-        wemo_port = None
-        logger.debug('Invalid IP address in device attributes')
+        return False
 
-    # If port was found, create url for device and run discovery function
-    if wemo_port is not None:
-        wemo_url = 'http://%s:%i/setup.xml' % (device.address, wemo_port)
-        logger.debug('Resulting URL: [%s]', wemo_url)
-        try:
-            wemo_device = pywemo.discovery.device_from_description(wemo_url, None)
-            logger.debug('[%s] discovery successful', device.name)
-            return wemo_device
-        except:
-            logger.debug('[%s] discovery failed', device.name)
+
+# pywemo wrapper API **********************************************************
+class WemoAPI(object):
+    """ Class and methods necessary to read items from a google calendar  """
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+        self.wemo_device = None
+        self.wemo_port = None
+        self.wemo_url = str()
+        self.wemo_known = []
+        self.result = None
+        self.status = str()
+
+
+    def wemo_discover(self, device):
+        """ discovers wemo device on network based upon known IP address """
+        if check_ipv4(device.address) is True:
+            logger.debug('Valid IP address provided')
+            # Attempt to discover wemo device
+            try:
+                self.wemo_device = None
+                self.wemo_port = pywemo.ouimeaux_device.probe_wemo(device.address)
+                self.logger.debug('Device discovered at port %s', self.wemo_port)
+            except:
+                self.wemo_port = None
+                self.logger.debug('Failed to discover port for [%s]', device.name)
+        else:
+            self.wemo_port = None
+            self.logger.debug('Invalid IP address in device attributes')
+        # If port was found, create url for device and run discovery function
+        if self.wemo_port is not None:
+            self.wemo_url = 'http://%s:%i/setup.xml' % (device.address, self.wemo_port)
+            self.logger.debug('Resulting URL: [%s]', self.wemo_url)
+            try:
+                self.wemo_device = pywemo.discovery.device_from_description(
+                    self.wemo_url,
+                    None)
+                self.logger.debug('[%s] discovery successful', device.name)
+                return self.wemo_device
+            except:
+                self.logger.debug('[%s] discovery failed', device.name)
+                return None
+        else:
             return None
-    else:
-        return None
 
 
-# Wemo status check function **************************************************
-async def wemo_read_status(device, wemo_list, logger):
-    # Wemo devices get a status query message sent to detect if they are
-    # on the network or not and what their current state is
-    logger.debug(
-        'Querrying device [%s] at [%s], original status [%s / %s]',
-        device.name,
-        device.address,
-        device.status,
-        device.last_seen)
-
-    # Check if device is already in the list of known wemo devices
-    result = next(
-        (index for index, wemodev in enumerate(wemo_list)
-         if wemodev.name == device.name), None)
-
-    # Point to existing list record or recently discovered device
-    if result == None:
-        wemo_device = await wemo_discover(device, logger)
-    else:
-        wemo_device = wemo_list[result]
-
-    # Perform status query
-    if wemo_device is not None:
-        status = str(wemo_device.get_state(force_update=True))
-        logger.debug(
-            'Wemo device [%s] found with status [%s]',
-            device.name, status)
-        # Re-define device record based on response from status query
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            device.cmd, device.cmd_mem, device.rule)
-        # If device was not previously in wemo list, add it for next time
-        if result == None:
-            wemo_list.append(copy.copy(wemo_device))
-    else:
-        status = 'offline'
-        logger.debug(
-            'Wemo device [%s] discovery failed.  Status set to [%s]',
-            device.name, status)
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            device.cmd, device.cmd_mem, device.rule)
-    return device, wemo_list
+    def wemo_read_status(self, device):
+        self.logger.debug(
+            'Querrying device [%s] at [%s], original status [%s / %s]',
+            device.name,
+            device.address,
+            device.status,
+            device.last_seen)
+        # Check if device is already in the list of known wemo devices
+        self.result = next(
+            (index for index, wemodev in enumerate(self.wemo_known)
+             if wemodev.name == device.name), None)
+        # Point to existing list record or recently discovered device
+        if self.result == None:
+            self.wemo_device = self.wemo_discover(device)
+        else:
+            self.wemo_device = self.wemo_known[self.result]
+        # Perform status query
+        if self.wemo_device is not None:
+            self.status = str(self.wemo_device.get_state(force_update=True))
+            self.logger.debug(
+                'Wemo device [%s] found with status [%s]',
+                device.name, self.status)
+            # Re-define device record based on response from status query
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                device.cmd, device.cmd_mem, device.rule)
+            # If device was not previously in wemo list, add it for next time
+            if self.result == None:
+                self.wemo_known.append(copy.copy(self.wemo_device))
+        else:
+            self.status = 'offline'
+            self.logger.debug(
+                'Wemo device [%s] discovery failed.  Status set to [%s]',
+                device.name, self.status)
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                device.cmd, device.cmd_mem, device.rule)
+        return device
 
 
-# Wemo set to on function *****************************************************
-async def wemo_set_on(device, wemo_list, logger):
-    # Wemo devices get a status query message sent to detect if they are
-    # on the network or not and what their current state is
-    logger.debug(
-        'Setting device [%s] at [%s], state to "on"',
-        device.name,
-        device.address)
-
-    # Check if device is already in the list of known wemo devices
-    result = next(
-        (index for index, wemodev in enumerate(wemo_list)
-         if wemodev.name == device.name), None)
-
-    # Point to existing list record or recently discovered device
-    if result == None:
-        logger.debug('Device not in wemo list.  Running discovery')
-        wemo_device = await wemo_discover(device, logger)
-    else:
-        logger.debug(
-            'Device already in wemo list as [%s]',
-            wemo_list[result])
-        wemo_device = wemo_list[result]
-
-    # Perform command, followed by status query
-    if wemo_device is not None:
-        wemo_device.on()
-        status = '1'
-        logger.debug(
-            '"on" command sent to wemo device [%s]', wemo_device.name)
-        # Re-define device record based on response from status query
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            device.cmd, device.cmd_mem, device.rule)
-        # If device was not previously in wemo list, add it for next time
-        if result == None:
-            wemo_list.append(copy.copy(wemo_device))
-    else:
-        status = 'offline'
-        logger.debug(
-            'Wemo device [%s] discovery failed.  Status set to [%s]',
-            device.name, status)
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            'on', device.cmd_mem, device.rule)
-    return device, wemo_list
+    # Wemo set to on function *****************************************************
+    def wemo_set_on(self, device):
+        """ Send 'turn on' command to a specific wemo device """
+        self.logger.debug(
+            'Setting device [%s] at [%s], state to "on"',
+            device.name,
+            device.address)
+        # Check if device is already in the list of known wemo devices
+        self.result = next(
+            (index for index, wemodev in enumerate(self.wemo_known)
+             if wemodev.name == device.name), None)
+        # Point to existing list record or recently discovered device
+        if self.result == None:
+            self.logger.debug('Device not in wemo list.  Running discovery')
+            self.wemo_device = self.wemo_discover(device)
+        else:
+            self.logger.debug(
+                'Device already in wemo list as [%s]',
+                self.wemo_known[self.result])
+            self.wemo_device = self.wemo_known[self.result]
+        # Perform command, followed by status query
+        if self.wemo_device is not None:
+            self.wemo_device.on()
+            self.status = '1'
+            self.logger.debug(
+                '"on" command sent to wemo device [%s]', self.wemo_device.name)
+            # Re-define device record based on response from status query
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                device.cmd, device.cmd_mem, device.rule)
+            # If device was not previously in wemo list, add it for next time
+            if self.result == None:
+                self.wemo_known.append(copy.copy(self.wemo_device))
+        else:
+            self.status = 'offline'
+            self.logger.debug(
+                'Wemo device [%s] discovery failed.  Status set to [%s]',
+                device.name, self.status)
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                'on', device.cmd_mem, device.rule)
+        return device
 
 
-# Wemo set to off function ****************************************************
-async def wemo_set_off(device, wemo_list, logger):
-    # Wemo devices get a status query message sent to detect if they are
-    # on the network or not and what their current state is
-    logger.debug(
-        'Setting device [%s] at [%s], state to "off"',
-        device.name,
-        device.address)
-
-    # Check if device is already in the list of known wemo devices
-    result = next(
-        (index for index, wemodev in enumerate(wemo_list)
-         if wemodev.name == device.name), None)
-
-    # Point to existing list record or recently discovered device
-    if result == None:
-        logger.debug('Device not in wemo list.  Running discovery')
-        wemo_device = await wemo_discover(device, logger)
-    else:
-        logger.debug(
-            'Device already in wemo list as [%s]',
-            wemo_list[result])
-        wemo_device = wemo_list[result]
-
-    # Perform command, followed by status query
-    if wemo_device is not None:
-        wemo_device.off()
-        status = '0'
-        logger.debug(
-            '"off" command sent to wemo device [%s]', wemo_device.name)
-        # Re-define device record based on response from status query
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            'off', device.cmd_mem, device.rule)
-        # If device was not previously in wemo list, add it for next time
-        if result == None:
-            wemo_list.append(copy.copy(wemo_device))
-    else:
-        status = 'offline'
-        logger.debug(
-            'Wemo device [%s] discovery failed.  Status set to [%s]',
-            device.name, status)
-        device = rpihome_v3.Device(
-            device.name, device.devtype, device.address,
-            status, device.status_mem, str(datetime.datetime.now()),
-            device.cmd, device.cmd_mem, device.rule)
-    return device, wemo_list
+    # Wemo set to off function ****************************************************
+    def wemo_set_off(self, device):
+        """ Send 'turn off' command to a specific wemo device """
+        self.logger.debug(
+            'Setting device [%s] at [%s], state to "off"',
+            device.name,
+            device.address)
+        # Check if device is already in the list of known wemo devices
+        self.result = next(
+            (index for index, wemodev in enumerate(self.wemo_known)
+             if wemodev.name == device.name), None)
+        # Point to existing list record or recently discovered device
+        if self.result == None:
+            self.logger.debug('Device not in wemo list.  Running discovery')
+            self.wemo_device = self.wemo_discover(device)
+        else:
+            self.logger.debug(
+                'Device already in wemo list as [%s]',
+                self.wemo_known[self.result])
+            self.wemo_device = self.wemo_known[self.result]
+        # Perform command, followed by status query
+        if self.wemo_device is not None:
+            self.wemo_device.off()
+            self.status = '0'
+            self.logger.debug(
+                '"off" command sent to wemo device [%s]', self.wemo_device.name)
+            # Re-define device record based on response from status query
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                'off', device.cmd_mem, device.rule)
+            # If device was not previously in wemo list, add it for next time
+            if self.result == None:
+                self.wemo_known.append(copy.copy(self.wemo_device))
+        else:
+            self.status = 'offline'
+            self.logger.debug(
+                'Wemo device [%s] discovery failed.  Status set to [%s]',
+                device.name, self.status)
+            device = rpihome_v3.Device(
+                device.name, device.devtype, device.address,
+                copy.copy(self.status), device.status_mem, str(datetime.datetime.now()),
+                device.cmd, device.cmd_mem, device.rule)
+        return device
