b4953aed510a182610e15fb1e7fcf86f49e62c80
Maue <chris.maue@intelligrated.com>
2017-05-24 10:05:18 -0500

    Retructured using a asyncio coroutine wrapper for function calls

--- run.py
+++ run.py
@@ -21,6 +21,29 @@ __email__ = "csmaue@gmail.com"
 __status__ = "Development"
 
 
+
+# Async wrapper function ******************************************************
+async def asyncio_wrapper(function, loop, sleep, logger):
+    """ test """
+    while True:
+        try:
+            # Perform desired function on args
+            function
+
+            # Do not loop when status flag is false
+            if loop is False:
+                logger.debug('Breaking out of update_device_status loop')
+                break
+            # Otherwise wait a pre-determined time period, then re-run the task
+            logger.debug('Sleeping task for %s seconds', str(sleep))
+            await asyncio.sleep(sleep)
+        except KeyboardInterrupt:
+            logging.debug(
+                'Killing task')
+            break
+            break
+
+
 # Main event loop function ****************************************************
 def main():
     """ main function for the rpihome application """
@@ -28,43 +51,42 @@ def main():
     # Get configuration from INI file for this run
     logger, credentials, database, devices = (
         rpihome_v3.configure_all('rpihome_v3//config.ini'))
-    logger.info(
-        'RpiHome-v3 Application Started @ [%s] ******************************',
-        str(datetime.datetime.now()))
-    logger.info('Configuration imported from INI file')
+    logger.info('Configuration imported from INI file *********************')
 
-    # Create empty list for wemo devices (helps prevent having to run
-    # multiple discoveries)
-    wemo = []
-    nest = []
-
-    # Create initial sunrise and sunset values before updating them
+    # Create various support objects
+    wemo = rpihome_v3.WemoAPI(logger)
     sun = rpihome_v3.Sun(38.566268, -90.409878, -5, logger)
-
-    # Create connection to calendar
-    cal = rpihome_v3.GoogleCalSync(
+    sched = rpihome_v3.GoogleCalSync(
         cal_id='r68pvu542kle1jm7jj9hjdp9o0@group.calendar.google.com',
         logger=logger)
+    nest = []
 
     # Get main event loop *****************************************************
     logger.info('Getting main event loop')
     event_loop = asyncio.get_event_loop()
-
-    # Perform initial schedule query ******************************************
-    #schedule = rpihome_v3.update_schedule(credentials, False, logger)
 
     # Run event loop until keyboard interrupt received ************************
     try:
         logger.info('Call run_until_complete on task list')
         event_loop.run_until_complete(
             asyncio.gather(
-                rpihome_v3.update_adevice_status(devices, wemo, True, logger),
-                rpihome_v3.update_pdevice_status(devices, True, logger),
-                rpihome_v3.update_mdevice_status(devices, True, logger),
-                #rpihome_v3.update_enviro_status(devices, nest, credentials, True, logger),
-                rpihome_v3.update_adevice_cmd(devices, wemo, cal, sun, True, logger),
-                rpihome_v3.update_database(database, devices, True, logger)
-                ))
+                asyncio_wrapper(
+                    rpihome_v3.update_adev_status(devices, wemo, logger),
+                    True, 5, logger),
+                asyncio_wrapper(
+                    rpihome_v3.update_pdev_status(devices, logger),
+                    True, 15, logger),
+                asyncio_wrapper(
+                    rpihome_v3.update_mdev_status(devices, logger),
+                    True, 2, logger),
+                asyncio_wrapper(
+                    rpihome_v3.update_adev_cmd(devices, wemo, cal, sched, logger),
+                    True, 5, logger),
+                asyncio_wrapper(
+                    rpihome_v3.update_database(database, devices, logger),
+                    True, 5, logger)
+                )
+            )
         logger.info('Tasks are started')
     except KeyboardInterrupt:
         logger.debug('Closing connection to database')
