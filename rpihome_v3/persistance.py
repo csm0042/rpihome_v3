29e6870b3b66972541f7ddac88fff0da2f7698ba
Maue <chris.maue@intelligrated.com>
2017-07-12 13:22:11 -0500

    Added pending cmd query to persistance module

--- rpihome_v3/persistance.py
+++ rpihome_v3/persistance.py
@@ -3,9 +3,15 @@
 """
 
 # Import Required Libraries (Standard, Third Party, Local) ********************
+import copy
+import datetime
 import logging
 import mysql.connector
 import mysql.connector.errorcode as errorcode
+import sys
+import time
+if __name__ == "__main__":
+    sys.path.append("..")
 import rpihome_v3
 
 
@@ -43,3 +49,47 @@ def insert_record(database, device, logger):
     except:
         logger.warning(
             'Attempt to inesrt record into database failed')
+
+
+def query_command(database, logger):
+    """ test """
+    # Configure logger
+    logger = logger or logging.getLogger(__name__)
+    # Attempt database record insert
+    try:
+        # Check first if valid database connection was made
+        result_list = []
+        if database is not None:
+            cursor = database.cursor()
+            query = ("SELECT id_device_cmd, device, cmd, timestamp, processed "
+                     "FROM device_cmd "
+                     "WHERE (timestamp >= '%s' AND processed IS NULL)")
+            current_time = (str(datetime.datetime.now() + datetime.timedelta(days=-30)))[:19]
+            data = (current_time)
+            cursor.execute(query, data)
+
+            row = cursor.fetchone()
+            while row is not None:
+                result_list.append(copy.copy(row))
+                print(row)
+                row = cursor.fetchone()
+
+        else:
+            logger.warning(
+                'No connection to database. Cannot perform query')
+    except:
+        logger.warning(
+            'Attempt to query database failed')
+    finally:
+        database.commit()
+        cursor.close() 
+
+
+
+if __name__ == "__main__":
+    # Get configuration
+    logger, credentials, location, tasks, database, devices = \
+        rpihome_v3.configure_application('config.ini')
+    while True:
+        query_command(database, logger)
+        time.sleep(0.25)
