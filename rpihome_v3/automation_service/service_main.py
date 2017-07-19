5565985193e6459677d2039e98330c32db055d24
Maue <chris.maue@intelligrated.com>
2017-07-19 14:06:39 -0500

    Message decoding and actions being coded - very much a work in progress right now

--- rpihome_v3/automation_service/service_main.py
+++ rpihome_v3/automation_service/service_main.py
@@ -41,9 +41,18 @@ def service_main_task(msg_in_que, msg_out_que, rNumGen, devices, log,
             next_msg_seg = next_msg.split(sep=',')
             msgHeader = next_msg_seg[:5]
             msgPayload = next_msg_seg[5:]
-            
-            # Do Stuff
-            
+
+            # Process messages from database service
+            if msgHeader[3] == db_add:
+                response_msg_list = process_db_messages(
+                    rNumGen, devices, msgHeader, msgPayload, log,
+                    address, port, db_add, db_port, wemo_add, wemo_port)
+
+            # Process messages from wemo service
+            if msgHeader[3] == wemo_add:
+                response_msg_list = process_wemo_messages(
+                    rNumGen, devices, msgHeader, msgPayload, log)
+           
         # Que up response messages in outgoing msg que
         if len(response_msg_list) > 0:
             log.debug('Queueing response message(s)')
@@ -53,3 +62,89 @@ def service_main_task(msg_in_que, msg_out_que, rNumGen, devices, log,
                           response_msg)
         # Yield to other tasks for a while
         yield from asyncio.sleep(0.25)
+
+
+# Process messages from database service **************************************
+def process_db_messages(rNumGen, devices, msgHeader, msgPayload, log,
+                        address, port, db_add, db_port, wemo_add, wemo_port):
+    """ This function performs the custom operations required when a message
+        is received from the db service """
+    # Initialize result list
+    response_msg_list = []        
+    # message type 101 - log status update ACK
+    if msgPayload[0] == '101':
+        pass
+    # Message type 103 - device command from DB
+    elif msgPayload[0] == '103':
+        # Find matching device in device list
+        address = str()
+        status = str()
+        lastSeen = str()
+        log.debug('Finding device in known device list with name: [%s]',
+                  msgPayload[1])
+        for d in devices:
+            if d.name.lower() == msgPayload[2].lower():
+                log.debug('Match found with address: [%s]', d.address)
+                address = d.address
+                status = d.status
+                lastSeen =  d.last_seen
+                break
+        if len(address) > 0 and (
+            msgPayload[3].lower() == 'on' or msgPayload[3].lower() == '1', or \
+            msgPayload[3].lower() == 'off' or msgPayload[3].lower() == '0'):
+            # Create message to wemo service to execute command
+            log.debug('Building message header for msg to wemo service')
+            response_header = rNumGen.new() + ',' + wemo_add + ',' + \
+                            wemo_port + ',' + address + ',' + \
+                            port
+            log.debug('Building message payload for msg to wemo service')
+            if msgPayload[3].lower() == "on" or msgPayload[3].lower() == '1':
+                response_payload = '102,' + msgPayload[2] + ',' + address + ',' + \
+                                   status + ',' + lastSeen
+            if msgPayload[3].lower() == 'off' or msgPayload[3].lower() == '0'
+                response_payload = '104,' + msgPayload[2] + ',' + address + ',' + \
+                                   status + ',' + lastSeen
+            log.debug('Building complete response message')                    
+            response_msg = response_header + ',' + response_payload
+            log.debug('Appending complete response message to result list: [%s]',
+                      response_msg)
+            response_msg_list.append(copy.copy(response_msg))
+            # Create message to db service to mark command as executed
+            log.debug('Building message header for msg to DB service')
+            response_header = rNumGen.new() + ',' + db_add + ',' + \
+                            db_port + ',' + address + ',' + \
+                            port
+            log.debug('Building message payload for msg to wemo service')
+            response_payload = '104,' + msgPayload[1] + ',' + address + ',' + \
+                               status + ',' + lastSeen
+            log.debug('Building complete response message')                    
+            response_msg = response_header + ',' + response_payload
+            log.debug('Appending complete response message to result list: [%s]',
+                    response_msg)
+            response_msg_list.append(copy.copy(response_msg))            
+        else:
+            log.warning('Unrecoginized device name in command received from DB')
+    # Message type 105 - device command from
+    elif msgPayload[0] == '105':
+        pass
+    # Return response message
+    return response_msg_list        
+
+
+# Process messages from wemo service ******************************************
+def process_wemo_messages(rNumGen, msgHeader, msgPayload, log):
+    """ This function performs the custom operations required when a message
+        is received from the wemo service """
+    # Initialize result list
+    response_msg_list = []        
+    # Message type 101 - wemo status update
+    if msgPayload[0] == '101':
+        pass
+    # Message type 103 - wemo set on ACK
+    elif msgPayload[0] == '103':
+        pass
+    # Message type 105 - wemo set off ACK
+    elif msgPayload[0] == '105':
+        pass
+    # Return response message
+    return response_msg_list        
